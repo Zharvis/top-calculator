6e985b1862f0ab05b9ff639d47e0555ad4e89eea
fmrc <fatih.mehmed.cin@gmail.com>
2022-07-22 12:36:05 +0200

    feat: add computing logic

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => (b === 0 ? "err: div w/null" : a / b);
+
+let operate = (operator, a, b) => {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    case "multiply":
+      return multiply(a, b);
+    case "divide":
+      return divide(a, b);
+    default:
+      return "err: wrong operator";
+  }
+};
